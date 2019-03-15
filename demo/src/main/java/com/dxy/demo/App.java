c11aa1e06dbbad5697c5afd98e15f7cd599cc843
duanhongyuan <duanhongyuan@csii.com.cn>
2019-03-15 17:29:59 +0800

    基础版本提交

---
+++ demo/src/main/java/com/dxy/demo/App.java
@@ -0,0 +1,13 @@
+package com.dxy.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
